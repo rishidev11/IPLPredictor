93671192379cbfc8fa2f0a745220d630a80fd34c
rishi <rishi.rgt@gmail.com>
2024-07-09 16:47:02 +0100

    Finished Backend

--- Predictor.py
+++ Predictor.py
@@ -1,5 +1,11 @@
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+import matplotlib.pyplot as plt
 
 matches = pd.read_csv('IPLMatches.csv')
 deliveries = pd.read_csv('deliveries.csv')
@@ -88,5 +94,90 @@ final_df = delivery_df[['batting_team', 'bowling_team', 'city', 'runs_left', 'ba
 
 # Shuffles the rows in the dataframe randomly to prevent any bias from forming
 final_df = final_df.sample(final_df.shape[0])
+
+final_df.dropna(inplace=True)
+final_df = final_df[final_df['balls_left'] != 0]
+
+# Selects all columns except the last one from final_df and assigns them to X.
+# This is typically done to separate the features (input variables) from the target variable.
+X = final_df.iloc[:,:-1]
+
+# Selects the last column from final_df and assigns it to y.
+# This is typically the target variable that you want to predict.
+y = final_df.iloc[:,-1]
+
+# Splits data into training and testing sets
+# 0.2 test_size means that 20% of the data will be used for testing, and 80% will be used for training.
+# random_state=1: This ensures that the split is reproducible.
+# If you run the code multiple times with the same random_state, you will get the same split.
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2, random_state=1)
+
+# A ColumnTransformer is used to apply different transformations to differnet columns of a dataframe
+# In this case, OneHotEncoding is applied to the batting_team, bowling_team and city columns
+trf = ColumnTransformer([
+    ('trf', OneHotEncoder(sparse_output=False, drop='first'), ['batting_team', 'bowling_team', 'city'])], remainder='passthrough')
+
+# Pipeline created to sequentially apply transformations to the data
+# First is the ColumnTransformer, next is the LogisticRegression model
+pipe = Pipeline(steps=[
+    ('step1', trf),
+    ('step2', LogisticRegression(solver='liblinear'))
+])
+
+# Pipeline fit to training data
+pipe.fit(X_train, y_train)
+
+# y_pred contains predictions made by the pipeline on the test set X_test.
+y_pred = pipe.predict(X_test)
+
+
+def match_summary(row):
+    print("Batting Team-" + row['batting_team'] + " | Bowling Team-" + row['bowling_team'] + " | Target- " + str(row['total_runs_x']))
+
+# Tracks progression of a match over each over, using the pipeline to show win/lose probabilities
+# Filters the DataFrame x_df for the given match_id and only considers the data at the end of each over (ball == 6).
+# Cleans the data and predicts win/lose probabilities for each over using pipe.
+# Calculates additional metrics like runs_after_over and wickets_in_over.
+# Prints the target score and returns a DataFrame with the progression details and the target score
+def match_progression(x_df, match_id, pipe):
+    match = x_df[x_df['match_id'] == match_id]
+    match = match[match['ball'] == 6]
+    temp_df = match[['batting_team', 'bowling_team', 'city', 'runs_left', 'balls_left', 'wickets', 'total_runs_x', 'crr','rrr']].dropna()
+    temp_df = temp_df[temp_df['balls_left'] != 0]
+    result = pipe.predict_proba(temp_df)
+    temp_df['lose'] = np.round(result.T[0] * 100, 1)
+    temp_df['win'] = np.round(result.T[1] * 100, 1)
+    temp_df['end_of_over'] = range(1, temp_df.shape[0] + 1)
+
+    target = temp_df['total_runs_x'].values[0]
+    runs = list(temp_df['runs_left'].values)
+    new_runs = runs[:]
+    runs.insert(0, target)
+    temp_df['runs_after_over'] = np.array(runs)[:-1] - np.array(new_runs)
+    wickets = list(temp_df['wickets'].values)
+    new_wickets = wickets[:]
+    new_wickets.insert(0, 10)
+    wickets.append(0)
+    w = np.array(wickets)
+    nw = np.array(new_wickets)
+    temp_df['wickets_in_over'] = (nw - w)[0:temp_df.shape[0]]
+
+    print("Target-", target)
+    temp_df = temp_df[['end_of_over', 'runs_after_over', 'wickets_in_over', 'lose', 'win']]
+    return temp_df, target
+
+
+temp_df,target = match_progression(delivery_df,1,pipe)
+
+plt.figure(figsize=(18,8))
+plt.plot(temp_df['end_of_over'],temp_df['wickets_in_over'],color='yellow',linewidth=3)
+plt.plot(temp_df['end_of_over'],temp_df['win'],color='#00a65a',linewidth=4)
+plt.plot(temp_df['end_of_over'],temp_df['lose'],color='red',linewidth=4)
+plt.bar(temp_df['end_of_over'],temp_df['runs_after_over'])
+plt.title('Target-' + str(target))
+
+
+
+
 
 
